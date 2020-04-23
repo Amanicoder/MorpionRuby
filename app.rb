0e347b933d2487e6ecfdbea3ca20a665cd327efd
Lebeil <liedel75@gmail.com>
2020-04-23 21:35:26 +0200

    final

--- app.rb
+++ app.rb
@@ -16,21 +16,20 @@ class BoardCase  #TO DO : la classe a 2 attr_accessor, sa valeur (X, O, ou vide)
 end
 
 class Board
-  #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases 
-  
+  #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases  
   attr_accessor :board
 
   def initialize    #TO DO :    #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
                                 #Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
-    @A1 = BoardCase.new    @A1 = 0
-    @A2 = BoardCase.new    @A2 = 1
-    @A3 = BoardCase.new    @A3 = 2    
-    @B1 = BoardCase.new    @B1 = 3     
-    @B2 = BoardCase.new    @B2 = 4     
-    @B3 = BoardCase.new    @B3 = 5
-    @C1 = BoardCase.new    @C1 = 6    
-    @C2 = BoardCase.new    @C2 = 7     
-    @C3 = BoardCase.new    @C3 = 8
+    @A1 = BoardCase.new @A1 = 0
+    @A2 = BoardCase.new @A2 = 1
+    @A3 = BoardCase.new @A3 = 2    
+    @B1 = BoardCase.new @B1 = 3     
+    @B2 = BoardCase.new @B2 = 4     
+    @B3 = BoardCase.new @B3 = 5
+    @C1 = BoardCase.new @C1 = 6    
+    @C2 = BoardCase.new @C2 = 7     
+    @C3 = BoardCase.new @C3 = 8
 
     # @board = [ @A1.to_s , @A2.to_s  , @A3.to_s  , @B1.to_s  , @B2.to_s  , @B3.to_s  , @C1.to_s  , @C2.to_s  , @C3.to_s  ]
     @board = [ @A1 , @A2 ,@A3 ,@B1 ,@B2 ,@B3 ,@C1 ,@C2 ,@C3 ]
@@ -147,7 +146,7 @@ class Game
         puts "Voulez-vous rejouer une partie? (Y/N)"
         réponse = gets.chomp.upcase
         if réponse == "Y"
-          Game.new.go
+          Game.new.go #on recommence
         elsif réponse =="N"
           exit
         else puts "Veuillez répondre par Y ou N "
